2e6ed26be09c90c50d22164228922ffbe2e6c190
Thomas Lekanger <thomas.lekanger@hotmail.com>
2025-04-05 16:44:29 +0200

    BELABOX install

--- install/belabox/install.sh
+++ install/belabox/install.sh
@@ -4,8 +4,12 @@ set -e
 
 WORKSPACE=moblink-rust-install
 REPO="datagutt/moblink-rust"
-LATEST_RELEASE_URL="https://github.com/$REPO/releases/latest/download"
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+# Get latest release tag name from GitHub API
+LATEST_TAG=$(wget -qO- https://api.github.com/repos/$REPO/releases/latest | grep -Po '"tag_name": "\K.*?(?=")')
+# Remove 'v' prefix for the folder name
+LATEST_VERSION=${LATEST_TAG#v}
+LATEST_RELEASE_URL="https://github.com/$REPO/releases/download/$LATEST_TAG"
+LATEST_RELEASE_SOURCE_CODE_URL="https://github.com/$REPO/archive/refs/tags/$LATEST_TAG.tar.gz"
 
 # Detect architecture
 ARCH=$(uname -m)
@@ -34,14 +38,17 @@ wget "$LATEST_RELEASE_URL/moblink-relay-$TARGET"
 wget "$LATEST_RELEASE_URL/moblink-relay-service-$TARGET"
 wget "$LATEST_RELEASE_URL/moblink-streamer-$TARGET"
 
+# Download systemd files from release (source code)
+wget "$LATEST_RELEASE_SOURCE_CODE_URL"
+tar -xzf "$LATEST_TAG.tar.gz"
+cp moblink-rust-$LATEST_VERSION/install/belabox/systemd/moblink-relay-service.service /etc/systemd/system/
+cp moblink-rust-$LATEST_VERSION/install/belabox/systemd/moblink-streamer.service /etc/systemd/system/
+
 # Make binaries executable and move to /usr/local/bin
 chmod +x moblink-relay-$TARGET moblink-relay-service-$TARGET moblink-streamer-$TARGET
 mv moblink-relay-$TARGET /usr/local/bin/moblink-relay
 mv moblink-relay-service-$TARGET /usr/local/bin/moblink-relay-service
 mv moblink-streamer-$TARGET /usr/local/bin/moblink-streamer
-
-# Use local systemd files
-cp "$SCRIPT_DIR/systemd/"* /etc/systemd/system/
 
 systemctl enable moblink-streamer
 systemctl start moblink-streamer
@@ -49,4 +56,5 @@ systemctl start moblink-streamer
 systemctl enable moblink-relay-service
 systemctl start moblink-relay-service
 
+cd -
 rm -rf $WORKSPACE
